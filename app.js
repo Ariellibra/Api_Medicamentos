996f61a604048f10a2fadfe3c8b92fc2b4135b6a
Ariellibra <ariel-plate@hotmail.com>
2025-05-28 13:15:20 -0300

    index agregado

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const fs = require("fs");
 const csv = require("csv-parser");
-const medicamentosRouter = require("../routes/medicamentos");
+const medicamentosRouter = require("./routes/medicamentos");
 
 const app = express();
 app.use(express.json());
