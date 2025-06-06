42cfa7ae4585055cf42d7b700b51c88ba4169e41
Ariellibra <ariel-plate@hotmail.com>
2025-06-06 19:24:57 -0300

    modal fix v 1.3

--- public/js/admin.js
+++ public/js/admin.js
@@ -146,6 +146,7 @@ function confirmarBorrado(id) {
     modal.classList.remove("d-none");
     modal.classList.add("d-flex");
     $("confirmarId").value = id;
+    cerrarConfirmacion()
 }
 
 function cerrarConfirmacion() {
